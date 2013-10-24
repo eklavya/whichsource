614ac751d3443341e55b82fd6ce120f6d10b2637
eklavya <saurabh.rawat90@gmail.com>
2013-10-24 11:37:40 +0530

    test cleanup

--- test/AskerSpec.scala
+++ test/AskerSpec.scala
@@ -15,6 +15,13 @@ import models._
 import akka.util.Timeout
 import Indexing._
 import play.api.templates.Html
+import DataProvider._
+
+class TestAsker extends Asker {
+  override def condHolds(conds: List[(String, String)]) = TestFunctions.holds(conds)
+
+  override def findFunc(f: String, l: String) = TestFunctions.findFunc(f, l.toInt)
+}
 
 class AskerSpec extends TestKit(ActorSystem("AskerSpec")) with WordSpec with MustMatchers with BeforeAndAfterAll {
   implicit val timeout = Timeout(5.seconds)
@@ -24,19 +31,13 @@ class AskerSpec extends TestKit(ActorSystem("AskerSpec")) with WordSpec with Mus
   }
 
   "Asker" should {
-    import DataProvider._
-    import Indexing._
 
     "return result in correct format when search is successful" in {
-      val i = TestActorRef(new Asker {
-        override def condHolds(conds: List[(String, String)]) = TestFunctions.holds(conds)
+      val i = TestActorRef[TestAsker]
 
-        override def findFunc(f: String, l: String) = TestFunctions.findFunc(f, l.toInt)
-        })
-      
       TestFunctions.load
       TestFunctions.add("test.Function.func", new Func("test.Function.func", 1, 10, Some("body"), "some jar"))
-      
+
       val f = i ? SearchFuncs(List(("test.Function.func", "8")))
       //hack to get proper class name
       f.value.get.get.getClass	 must be(classOf[GotIt])
@@ -47,11 +48,7 @@ class AskerSpec extends TestKit(ActorSystem("AskerSpec")) with WordSpec with Mus
     }
 
     "return result in correct format when search is unsuccessful due to line number mismatch" in {
-      val i = TestActorRef(new Asker {
-        override def condHolds(conds: List[(String, String)]) = TestFunctions.holds(conds)
-
-        override def findFunc(f: String, l: String) = TestFunctions.findFunc(f, l.toInt)
-        })
+      val i = TestActorRef[TestAsker]
 
       val f = i ? SearchFuncs(List(("test.Function.func", "11")))
       //hack to get proper class name
@@ -60,11 +57,7 @@ class AskerSpec extends TestKit(ActorSystem("AskerSpec")) with WordSpec with Mus
     }
 
     "return result in correct format when search is unsuccessful due to function being unavailable" in {
-      val i = TestActorRef(new Asker {
-        override def condHolds(conds: List[(String, String)]) = TestFunctions.holds(conds)
-
-        override def findFunc(f: String, l: String) = TestFunctions.findFunc(f, l.toInt)
-        })
+      val i = TestActorRef[TestAsker]
 
       val f = i ? SearchFuncs(List(("test.Function.func1", "9")))
       //hack to get proper class name
